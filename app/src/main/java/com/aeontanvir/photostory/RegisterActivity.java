08bcbe999ffdfb2025ca30c55213fbb35d38ad37
Aeon Tanvir <aeontanvir@gmail.com>
2018-05-28 01:28:37 +0600

    Init commit

--- app/src/main/java/com/aeontanvir/photostory/RegisterActivity.java
+++ app/src/main/java/com/aeontanvir/photostory/RegisterActivity.java
@@ -2,12 +2,37 @@ package com.aeontanvir.photostory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText reg_email_field;
+    private EditText reg_pass_field;
+    private EditText reg_confirm_pass_field;
+    private Button reg_btn;
+    private Button reg_login_btn;
+    private ProgressBar reg_progress;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        reg_email_field = findViewById(R.id.reg_email);
+        reg_pass_field = findViewById(R.id.reg_pass);
+        reg_confirm_pass_field = findViewById(R.id.reg_confirm_pass);
+        reg_btn = findViewById(R.id.reg_btn);
+        reg_login_btn = findViewById(R.id.reg_login_btn);
+        reg_progress = findViewById(R.id.reg_progress);
     }
 }
